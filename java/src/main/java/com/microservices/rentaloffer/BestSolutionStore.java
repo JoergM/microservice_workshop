6baa5871a8a51cec7b362a6dbfbabcb6f8186546
Joerg Mueller <joergm42@gmail.com>
2015-12-02 13:08:14 +0100

    Lunchbreak

--- java/src/main/java/com/microservices/rentaloffer/BestSolutionStore.java
+++ java/src/main/java/com/microservices/rentaloffer/BestSolutionStore.java
@@ -1,9 +1,11 @@
 package com.microservices.rentaloffer;
 
 import com.google.gson.Gson;
+import com.google.gson.internal.Streams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static com.microservices.rentaloffer.SolutionType.ALTERNATIVE;
@@ -12,6 +14,7 @@ public class BestSolutionStore implements MessageHandler {
 
     protected static Logger logger = LoggerFactory.getLogger(BestSolutionStore.class);
     private static Connections connection;
+    private final HashMap<String, String> solutionMap;
 
     public static void main(String[] args) {
         String host = args[0];
@@ -21,29 +24,22 @@ public class BestSolutionStore implements MessageHandler {
         connection.deliveryLoop(new BestSolutionStore());
     }
 
+    public BestSolutionStore() {
+        solutionMap = new HashMap<>();
+    }
+
     public void handle(String message) {
         NeedPacket needPacket = new Gson().fromJson(message, NeedPacket.class);
 
-        if (doesNotAlreadyContainSolution(needPacket)) {
-            Solution solution = new Solution();
-            solution.setType(ALTERNATIVE);
-            solution.setSolution("Alternative");
-
-            needPacket.proposeSolution(solution);
-            connection.publish(needPacket.toJson());
-        }
-
+        Solution bestSolution = findBestSolution(needPacket);
+        solutionMap.put(needPacket.id, bestSolution.getSolution());
+        logger.info("Stored Best Solution for " + needPacket.id + " " + bestSolution.getSolution());
     }
 
-    private boolean doesNotAlreadyContainSolution(NeedPacket needPacket) {
-        List<Solution> solutions = needPacket.getSolutions();
-
-        for (Solution solution : solutions) {
-            if (solution.getType() == ALTERNATIVE) {
-                return false;
-            }
-        }
-        return true;
+    private Solution findBestSolution(NeedPacket needPacket) {
+        if (needPacket.getSolutions().size() > 0) {
+            return needPacket.getSolutions().get(0);
+        } else return null;
     }
 
 }
